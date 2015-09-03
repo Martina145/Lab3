e0296d5f58b596a7856520056476983d6e632762
Martina Sandberg <Martini@Martina-Sandbergs-MacBook-Pro.local>
2015-09-03 18:47:44 +0200

    added euclidian function

---
+++ Lab3.R
@@ -0,0 +1,13 @@
+euclidian <- function(x,y) {
+  if ( is.numeric(x) != TRUE || is.numeric(y) != TRUE) {
+    stop("Not numeric arguments")
+  }
+  while (x != y) {
+    if (x > y) {
+      x <- x - y
+    } else {
+      y <- y - x
+    }
+  }
+  return(x)
+}
