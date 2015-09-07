d6a2a4fd4c62a174615a4e76fc59d57ebac2c078
Martina Sandberg <Martini@c-212-107.eduroam.liu.se>
2015-09-07 16:57:32 +0200

    Documented wiki_graph

---
+++ Lab3/R/wiki_graph.R
@@ -0,0 +1,17 @@
+#' Nodes and weights
+#'
+#' A dataset containing nodes and wieghts for a graph. 
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item v1 vector with nodes 
+#'   \item v2 vector with nodes
+#'   \item w vector with weights
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name wiki_graph
+#' @usage data(wiki_graph)
+#' @format A data frame with 18 rows and 3 variables
+NULL
