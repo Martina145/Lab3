f87c8224cd4432952ae0feda74b5a060b7b60a91
Martina Sandberg <Martini@Martina-Sandbergs-MacBook-Pro.local>
2015-09-08 17:17:06 +0200

    Början på test

---
+++ Lab3/tests/testthat/test_euclidean.R
@@ -0,0 +1,11 @@
+
+
+test_that("Return right value", {
+expect_that(euclidean(123612, 13892347912),equals(4))
+expect_that(euclidean(100, 1000),equals(100))
+})
+
+test_that("Return right error", {
+  expect_that(euclidean(2,q),
+              throws_error("Not numeric arguments"))
+})
